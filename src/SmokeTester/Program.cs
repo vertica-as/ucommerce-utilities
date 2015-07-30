96360b3c6d32b7ad8ac8c597ab5465b47e5a5875
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-07-30 09:20:47 +0200

    Use official uCommerce core package.

---
+++ src/SmokeTester/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UCommerce.EntitiesV2;
+using Vertica.UCommerce.Utilities.Testing.Builders;
+
+namespace SmokeTester
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Country country = new CountryBuilder();
+
+			Console.WriteLine(country.Culture);
+		}
+	}
+}
--- src/Utilities/Infrastructure/Extensions/RepeatExtensions.cs
+++ src/Utilities/Infrastructure/Extensions/RepeatExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Vertica.UCommerce.Utilities.Infrastructure.Extensions
